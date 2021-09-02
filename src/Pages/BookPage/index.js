e43a68b972f01ee0ab5e8886571ceac01272027c
GuiCoelho-S <gs.coelho@outlook.com>
2021-09-02 17:03:17 -0300

    site 02:in production

--- src/Pages/BookPage/index.js
+++ src/Pages/BookPage/index.js
@@ -1,80 +1,235 @@
-import Tel from '../../assets/icons/tel-icon.png';
-import Email from '../../assets/icons/email-icon.png'
-
-import React from 'react';
-import { motion } from 'framer-motion'
-import * as S from './style'
-
+import { motion } from "framer-motion";
+import * as S from "./style";
 
 const BookPage = () => {
-    return (
-        <motion.div 
-        initial={{opacity:0, scale:0.8}}
-        animate={{opacity:1, scale:1}}
-        transition={{delay:0.2, duration:1.2 }}
-        >
-        <S.Container>
-            <S.Content>
-                <S.Header>
+  return (
+    <motion.div
+      initial={{ opacity: 0, scale: 0.8 }}
+      animate={{ opacity: 1, scale: 1 }}
+      transition={{ delay: 0.2, duration: 1.2 }}
+    >
+      <S.Container>
+        <S.Content>
+          <S.Header></S.Header>
+          <S.C_Apresentation>
+            <S.CardMain>
+              <S.CardMainItem>
+                <div>
+                  <h3>Item01</h3>
+                  <p>OCTOBER 22,2017</p>
+                </div>
+              </S.CardMainItem>
+              <S.CardMainItem>
+                <div>
+                  <h3>Item02</h3>
+                  <p>January 12,2017</p>
+                </div>
+              </S.CardMainItem>
+              <S.CardMainItem>
+                <div>
+                  <h3>Item03</h3>
+                  <p>OCTOBER 22,2019</p>
+                </div>
+              </S.CardMainItem>
+            </S.CardMain>
+            <S.Apresentation>
+              <h2>Lorem ipsum dolor si a met</h2>
+              <p>
+                Nam libero tempore, cum soluta nobis est eligendi optio cumque
+                nihil impedit quo minus id quod maxime placeat facere possimus,
+                omnis voluptas assumenda est, omnis dolor repellendus.
+              </p>
+              <S.FlexColumn>
+                <article>Learn More</article>
+                <S.CircleButton>
+                  <S.IconPlay />
+                </S.CircleButton>
+              </S.FlexColumn>
+            </S.Apresentation>
+          </S.C_Apresentation>
+        </S.Content>
+        <S.Details>
+          <h2>Loren ipsum dolor si a met</h2>
+          <p>
+            At vero eos et accusamus et iusto odio dignissimos ducimus qui
+            blanditiis praesentium voluptatum deleniti atque corrupti quos
+            dolores et quas molestias excepturi sint occaecati
+          </p>
+          <aside>
+            <S.SquareButton>
+              <S.IconLeft />
+            </S.SquareButton>
+            <S.SquareButton>
+              <S.IconRight />
+            </S.SquareButton>
+          </aside>
+        </S.Details>
+        <S.Main>
+          <S.Title>
+            <h2>EXPERIENCE</h2>
+          </S.Title>
+          <S.CardVideo>
+            <S.FlexColumn>
+              <div>
+                <h1>That's item 01</h1>
+                <p>
+                  Curabitur finibus sapien felis, non luctus odio elementum
+                  pulvinar. Aliquam{" "}
+                </p>
+              </div>
+            </S.FlexColumn>
 
-                </S.Header>
-                <S.Apresentation>
+            <S.FlexColumn>
+              <article>
+                <h1>That's item 02</h1>
+                <p>
+                  Curabitur finibus sapien felis, non luctus odio elementum
+                  pulvinar. Aliquam{" "}
+                </p>
+              </article>
+            </S.FlexColumn>
 
-                </S.Apresentation>
-                <S.CardMain>
+            <S.FlexColumn>
+              <aside>
+                <h1>That's item 03</h1>
+                <p>
+                  Curabitur finibus sapien felis, non luctus odio elementum
+                  pulvinar. Aliquam{" "}
+                </p>
+              </aside>
+            </S.FlexColumn>
+          </S.CardVideo>
+        </S.Main>
+
+        <S.MainVideo>
+          <article>
+            <h1>
+              <center>GREAT EXPERIENCES FOR HOTEL AND RESORT</center>
+            </h1>
+            <div>
+              <p>CLICK THE PLAYER TO</p>
+              <S.CircleButton>
+                <S.IconPlay />
+              </S.CircleButton>
+              <p>WATCH THE OVERVIEW</p>
+            </div>
+          </article>
+        </S.MainVideo>
+
+        <S.Main>
+          <S.Title>
+            <h2>PRICES</h2>
+          </S.Title>
+          <S.TablePrice>
+            <S.ItemDetails>
+              <h3>Room1</h3>
+              <p>$ 50.00/ night</p>
+
+              <span>05 REVIEWS</span>
+              <span>03 GUESTS</span>
+              <p>23 km</p>
+
+              <div>
+                <p>
+                  Orci sagittis eu volutpat odio facilisis mauris sit. Eget
+                  gravida cum sociis natoque penatibus et magnis dis. Fringilla
+                  est ullamcorper eget nulla facilisi. Tortor at auctor urna
+                  nunc id cursus metus aliquam. Nunc non blandit massa enim nec
+                  dui nunc mattis enim. Sit amet consectetur adipiscing elit. Id
+                  cursus metus aliquam eleifend mi in nulla posuere.
+                </p>
+              </div>
+            </S.ItemDetails>
+            <S.ItemDetails>
+              <h3>Room1</h3>
+              <p>$ 50.00/ night</p>
+
+              <span>05 REVIEWS</span>
+              <span>03 GUESTS</span>
+              <p>23 km</p>
+
+              <div>
+                <p>
+                  Orci sagittis eu volutpat odio facilisis mauris sit. Eget
+                  gravida cum sociis natoque penatibus et magnis dis. Fringilla
+                  est ullamcorper eget nulla facilisi. Tortor at auctor urna
+                  nunc id cursus metus aliquam. Nunc non blandit massa enim nec
+                  dui nunc mattis enim. Sit amet consectetur adipiscing elit. Id
+                  cursus metus aliquam eleifend mi in nulla posuere.
+                </p>
+              </div>
+            </S.ItemDetails>
+            <S.ItemDetails>
+              <h3>Room1</h3>
+              <p>$ 50.00/ night</p>
+
+              <span>05 REVIEWS</span>
+              <span>03 GUESTS</span>
+              <p>23 km</p>
+
+              <div>
+                <p>
+                  Orci sagittis eu volutpat odio facilisis mauris sit. Eget
+                  gravida cum sociis natoque penatibus et magnis dis. Fringilla
+                  est ullamcorper eget nulla facilisi. Tortor at auctor urna
+                  nunc id cursus metus aliquam. Nunc non blandit massa enim nec
+                  dui nunc mattis enim. Sit amet consectetur adipiscing elit. Id
+                  cursus metus aliquam eleifend mi in nulla posuere.
+                </p>
+              </div>
+            </S.ItemDetails>
+            <S.ItemDetails>
+              <h3>Room1</h3>
+              <p>$ 50.00/ night</p>
+              <p>05 REVIEWS</p>
+              <p>03 GUESTS</p>
+              <p>23 km</p>
+
+              <div>
+                <p>
+                  Orci sagittis eu volutpat odio facilisis mauris sit. Eget
+                  gravida cum sociis natoque penatibus et magnis dis. Fringilla
+                  est ullamcorper eget nulla facilisi. Tortor at auctor urna
+                  nunc id cursus metus aliquam. Nunc non blandit massa enim nec
+                  dui nunc mattis enim. Sit amet consectetur adipiscing elit. Id
+                  cursus metus aliquam eleifend mi in nulla posuere.
+                </p>
+              </div>
+            </S.ItemDetails>
+          </S.TablePrice>
+        </S.Main>
+        <S.Main>
+          <S.Title>
+            <h5>THE BEST OUR</h5>
+            <h2>ROOMS</h2>
+          </S.Title>
+          <S.Room>
+            <S.ContentRoom>
+              <aside></aside>
+              <section>
+                <h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit</h2>
+                <p>
+                  Orci sagittis eu volutpat odio facilisis mauris sit. Eget
+                  gravida cum sociis natoque penatibus et magnis dis. Fringilla
+                  est ullamcorper eget nulla facilisi..
+                </p>
+                <S.FlexColumn>
+                    <S.Images />
+                    <S.Images />
+                    <S.Images />
+                    <S.Images />
                     
-                </S.CardMain>
-            </S.Content>  
-            <S.Details>
-            
-            </S.Details>
-            <S.Main>
-                <S.CardVideo>
-                    <S.FlexColumn>
-                        <div>
-                            <h1>That's item 01</h1>
-                            <p>Curabitur finibus sapien felis, non luctus odio elementum pulvinar. Aliquam  </p>
-                        </div>
-                        
-                    </S.FlexColumn>
-
-                    <S.FlexColumn>
-                        <article>
-                        <h1>That's item 02</h1>
-                        <p>Curabitur finibus sapien felis, non luctus odio elementum pulvinar. Aliquam  </p>
-                        </article>
-                    </S.FlexColumn>
-
-                    <S.FlexColumn>
-                        <aside>
-                            <h1>That's item 03</h1>
-                            <p>Curabitur finibus sapien felis, non luctus odio elementum pulvinar. Aliquam  </p>
-                        </aside>
-                    </S.FlexColumn>
-                </S.CardVideo>
-            </S.Main>
-
-            <S.MainVideo>
-                    <article>
-                    
-                    <h1><center>GREAT EXPERIENCES FOR HOTEL AND RESORT</center></h1>
-                    <div>
-                        <p>CLICK THE PLAYER TO</p>
-                        <p>BUTAO</p>
-                        <p>WATCH THE OVERVIEW</p>
-                    </div>
-                    </article>
-                
-                
-            </S.MainVideo>
-
-            <S.Main>
-                
-            </S.Main>           
-        </S.Container>
-        
-        </motion.div>
-    );
+                </S.FlexColumn>
+                <S.CircleButton>
+                <S.IconPlay />
+              </S.CircleButton>
+              </section>
+            </S.ContentRoom>
+          </S.Room>
+        </S.Main>
+      </S.Container>
+    </motion.div>
+  );
 };
 
 export default BookPage;
--- src/Pages/BookPage/style.js
+++ src/Pages/BookPage/style.js
@@ -1,110 +1,373 @@
-import VideoBooks from '../../assets/images/BookPage/4.jpg';
+import VideoBooks from "../../assets/images/BookPage/4.jpg";
+import MainImage from "../../assets/images/BookPage/1.jpg"
+import KeyBook from "../../assets/images/BookPage/5.1.jpg"
 
-import styled from 'styled-components';
+import Play from "../../assets/icons/play-solid.svg";
+import Left from "../../assets/icons/angle-left-solid.svg";
+import Right from "../../assets/icons/angle-right-solid.svg";
+
+import styled from "styled-components";
+
+/* GLobal components*/
+export const Images = styled.img`
+width: 100%;
+height:100%;
+background-image: url(${KeyBook});
+
+`
+
+export const Icon = styled.div`
+  width: 50%;
+  height: 50%;
+  background-repeat: no-repeat;
+  background-position: 50% 50%;
+
+  filter: invert(1);
+`;
+export const IconPlay = styled(Icon)`
+  background-image: url(${Play});
+  
+`;
+export const IconLeft = styled(Icon)`
+  background-image: url(${Left});
+`;
+export const IconRight = styled(Icon)`
+  background-image: url(${Right});
+`;
+export const CircleButton = styled.button`
+  cursor: pointer;
+  object-fit: contain;
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+  background-color: #c18741;
+  border: none;
+  transition: all 0.3s;
+  display:grid;
+  place-items:center;
+
+  &:hover {
+    scale: 1.1;
+  }
+`;
+export const SquareButton = styled(CircleButton)`
+  border-radius: 0;
+`;
+/*********************/
+
 
 export const Container = styled.div`
-  margin:0 auto;
-  max-width:1280px;
-  height:100%;
-  display:flex;
-  flex-direction:column;
-  color:white;
+  margin: 0 auto;
+  max-width: 1280px;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  color: white;
 `;
 
-export const FlexColumn = styled.article`display:flex; flex-direction:Column;`
+export const FlexColumn = styled.article`
+  display: flex;
+  flex-direction: Column;
+`;
 
 export const Content = styled.div`
-width:100%;
-height:300px;
-display:grid;
-background-color: antiquewhite;
-grid-template-columns: repeat(2, 1fr);
-
-`
-
-export const Header = styled.header`
-background-color:pink;
-height:30px;
-grid-area: 1/1/2/3;
-`
-
-export const Apresentation = styled.article`
-background-color:yellow;
-height:200px;
-`
-export const CardMain = styled.aside`
-background-color:green;
-
-`
-export const Details = styled.aside`
-width:50%;
-border:1px solid blue;
-align-self:end;
-height:200px;
-`
+  width: 100%;
+  display: grid;
+  background-color: antiquewhite;
+  grid-template-rows: minmax(60px, auto)  500px;
+  grid-template-columns: 50%;
+`;
 
 export const Main = styled.div`
-display:grid;
-place-items:center;
-padding:30px 0 0 0;
-`
-export const MainVideo = styled.div`
-display:grid;
-border:1px solid pink;
-height:700px;
-align-items:center;
-justify-content:center;
-background-image: url(${VideoBooks});
+  display: grid;
+  place-items: center;
+  width:100%;
+  padding: 20px 0 20px 0;
+  color: black;
+`;
 
-article {
-    display:flex;
-    flex-direction:column;
-    justify-content:space-evenly;
-    height:200px;
-    width:600px;
-    border:1px solid red;
-    
-    div {
-        width:100%;
-        display:flex;
-        flex-direction:row;
-        justify-content:space-evenly;
-        align-self:center;
+export const Header = styled.header`
+  background-color: pink;
+  grid-area: 1/1/2/3;
+`;
+
+export const C_Apresentation = styled.main`
+ grid-area:2/1/2/3;
+ display:flex;
+ justify-content: space-between;
+ align-items:center;
+ background-image: url(${MainImage})
+`
+
+export const CardMain = styled.aside`
+  align-self:end;
+  width:500px;
+  height:300px;
+  transform:translateY(50%);
+  display:flex;
+  flex-direction:row;
+  justify-content: space-evenly;
+
+  
+`;
+
+export const CardMainItem = styled.article`
+width:160px;
+height:100%;
+background-image: url(${KeyBook});
+object-fit:contain;
+opacity:0.7;
+display:flex;
+align-items:end;
+border-radius:18px;
+div {
+    width:100%;
+    padding-bottom:40px;
+    text-align:center;
+    color:blanchedalmond;
+}`
+
+export const Apresentation = styled.div`
+  padding:40px 20px 10px 10px;
+  width:420px;
+  height:300px;
+  display:flex;
+  flex-direction:column;
+  align-items:flex-start;
+  justify-content: space-between;
+  
+  ${FlexColumn}{
+    flex-direction:row;
+    justify-content: space-between;
+    width:230px;
+    padding:0;
+    article{
+        width:160px;
+        display:grid;
+        place-items:center;
+        color:white;
+        font-size:16px;
+        padding-right:8px;
+        background-color:#c18741;
     }
+  }
+`;
+export const Details = styled.aside`
+  width: 400px;
+  align-self: end;
+  height: auto;
+  color:black;
+  display:flex;
+  flex-direction:column;
+  align-items:end;
+  justify-content: space-evenly;
+  padding:20px;
+  
+  h2 {
+      font-size:20px;
+  }
+  p{
+    padding:10px 8px;
+    font-size:14px;
+  }
+      
+  
+  aside { 
+      width:120px;
+      height:auto;
+      display:flex;
+      justify-content: space-between;
+  }
+`;
+
+
+export const Title = styled.div`
+  padding-top: 60px;
+  border-bottom: 5px solid #fa621a;
+  width: 300px;
+
+  * {
+      text-align:center;
+  }
+  h2 {
+    
+    padding-bottom: 10px;
+    font-size: 30px;
+  }
+`;
+
+export const MainVideo = styled.div`
+  display: grid;
+  border: 1px solid pink;
+  height: 700px;
+  align-items: center;
+  justify-content: center;
+  background-image: url(${VideoBooks});
+
+  article {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-evenly;
+    height: 200px;
+    width: 600px;
+    border: 1px solid red;
+
+    div {
+      width: 100%;
+      display: flex;
+      flex-direction: row;
+      justify-content: space-evenly;
+      align-self: center;
+      align-items:center;
+    }
+  }
+`;
+
+export const CardVideo = styled(FlexColumn)`
+  flex-direction: row;
+  width: 900px;
+  height: 300px;
+  z-index: 10;
+  transform: translateY(100px);
+
+  ${FlexColumn} {
+    flex-direction: column-reverse;
+    width: 100%;
+    background-image: url(${VideoBooks});
+    * {
+      height: 50%;
+
+      width: 100%;
+      padding: 8px 8px 3px 3px;
+      display: flex;
+      flex-direction: column;
+      justify-content: space-evenly;
+      align-items: end;
+    }
+    div {
+      background-color: #c0852a;
+      color: white;
+    }
+    article {
+      background-color: white;
+    }
+    aside {
+      background-color: whitesmoke;
+    }
+  }
+`;
+
+export const TablePrice = styled.section`
+padding:50px 0 0 0;
+display:grid;
+
+align-self:center;
+width:100%;
+gap:16px;
+justify-content: space-evenly;
+align-content: start;
+grid-template-columns: repeat(auto-fit,minmax(auto,300px));
+
+`
+
+export const ItemDetails = styled(FlexColumn)`
+background-image: url(${KeyBook});
+width:100%;
+height:140px;
+justify-content: space-evenly;
+background-color: white;
+opacity:0.6;
+color:black;
+transition:all 0.6s;
+align-self:start;
+border:1px solid red;
+align-items:center;
+
+div {
+    display:none;
+    width:100%;
+    padding: 5px 10px;
+
+    p {
+        text-align:center;
+    }
+}
+
+&:hover{
+    div {
+        display:block;
+    }
+    background-image:none;
+    background-color:#c18741;
+    color:white;
+    height:500px;
+    opacity:1;
+    
 }
 `
 
-export const CardVideo = styled(FlexColumn)`
-flex-direction:row;
-width:900px;
-height:300px;
-z-index:10;
-transform:translateY(100px);
+export const Room = styled.div`
+padding-top:60px;
+border:1px solid red;
+width:100%;
+height:600px;
+transform:translateY(160px);
+display:grid;
+place-items:center;
+position:relative;
+background-image: url(${MainImage});
+`
+export const ContentRoom = styled.article`
+margin:0 auto;
+border:1px solid blue;
+min-height:500px;
+width:100%;
+height:100%;
+max-width:900px;
+position:absolute;
+transform:translateY(-100px);
+border-radius:80px;
+display:flex;
 
-${FlexColumn}{
-    flex-direction:column-reverse;
-    width:100%;
-    background-image:url(${VideoBooks});
-    * {
-        height:50%;
-        
+aside {
+    width:40%;
+    background-image: url(${VideoBooks});
+    border-radius:80px 0 0 80px;
+}
+section {
+    width:60%;
+    background-color:white;
+    border-radius:0px 80px 80px 0px;
+    display:flex;
+    flex-direction:column;
+    justify-content: space-between;
+    padding: 30px 20px; 
+    ${FlexColumn}{
+        flex-wrap:wrap;
+        flex-direction:row;
         width:100%;
-        padding:8px 8px 3px 3px;
-        display:flex;
-        flex-direction: column;
-        justify-content:space-evenly;
-        align-items: end;
+        border:1px solid red;
+        justify-content: space-between;
+        ${Images}{
+            width:120px;
+            height:100px;
+            object-fit:contain;
+        }
     }
-    div {
-        background-color:#c0852a;
-        
+    ${CircleButton}{
+    
+        align-self:end;
+        justify-self:end;
+        bottom:0;
+        position: relative;;
+        width:82px;
+        height:82px;
+        ${IconPlay}{
+            background-color: transparent !important;
+            width:50%;
+        }
+       
     }
-    article {
-        background-color:white;
-        color:black;
-    }
-    aside {
-        background-color:whitesmoke;
-        color:black;
-    }
-}`
+}
+    
+`
